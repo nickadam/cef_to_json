0df04a4b2e9ddb66a0f688033009560b8f02a070
Nick Vissari <networknerd@gmail.com>
2021-08-12 15:10:34 -0400

    Build

--- dist/cef.js
+++ dist/cef.js
@@ -17,10 +17,12 @@ function toJson(cefOrig) {
   var ret = {};
 
   if (cefVerPos > 0) {
-    ret.headerPrefix = cefOrig.substring(0, cefVerPos).trim();
+    ret.prefix = cefOrig.substring(0, cefVerPos).trim();
   }
 
-  ret.cefVersion = cef.charAt(4);
+  ret.header = {};
+
+  ret.header.version = cef.charAt(4);
   var headers = cef.split('|');
   if (headers.length < 7) {
     return undefined;
@@ -32,12 +34,12 @@ function toJson(cefOrig) {
 
   var _headers$slice2 = _slicedToArray(_headers$slice, 6);
 
-  ret.deviceVendor = _headers$slice2[0];
-  ret.deviceProduct = _headers$slice2[1];
-  ret.deviceVersion = _headers$slice2[2];
-  ret.deviceEventClassId = _headers$slice2[3];
-  ret.name = _headers$slice2[4];
-  ret.agentSeverity = _headers$slice2[5];
+  ret.header.device_vendor = _headers$slice2[0];
+  ret.header.device_product = _headers$slice2[1];
+  ret.header.device_version = _headers$slice2[2];
+  ret.header.device_event_class_id = _headers$slice2[3];
+  ret.header.name = _headers$slice2[4];
+  ret.header.severity = _headers$slice2[5];
 
 
   if (headers.length === 7) {
@@ -46,7 +48,7 @@ function toJson(cefOrig) {
 
   // extracting extension fields which are in key=value format
   var re = /(\w*)=/g;
-  var str = headers[7];
+  var str = headers[7].replace(/\\=/g, 'THERE_IS_AN_EQUAL_SIGN_HERE_BRO');
   var exts = [];
   var m = void 0;
   /*eslint-disable*/
@@ -55,6 +57,10 @@ function toJson(cefOrig) {
   }
   /* eslint-enable */
 
+  if (exts.length > 0) {
+    ret.extension = {};
+  }
+
   exts.forEach(function (ext, index) {
     var nextPos = void 0;
     if (index === exts.length - 1) {
@@ -62,8 +68,27 @@ function toJson(cefOrig) {
     } else {
       nextPos = exts[index + 1].pos;
     }
-    ret[ext.key] = str.substring(ext.pos + ext.key.length + 1, nextPos).trim();
+    ret.extension[ext.key.replace(/THERE_IS_AN_EQUAL_SIGN_HERE_BRO/g, '=')] = str.substring(ext.pos + ext.key.length + 1, nextPos).trim().replace(/THERE_IS_AN_EQUAL_SIGN_HERE_BRO/g, '=');
   });
+
+  // replace labels
+  if (ret.extension) {
+    var keys = Object.keys(ret.extension);
+    var labels = keys.filter(function (x) {
+      return (/Label$/.test(x)
+      );
+    });
+    labels.forEach(function (label) {
+      var valueKey = label.replace(/Label$/, '');
+      if (keys.indexOf(valueKey) !== -1) {
+        var key = ret.extension[label].replace(/"/g, '');
+        var value = ret.extension[valueKey];
+        ret.extension[key] = value;
+        delete ret.extension[label];
+        delete ret.extension[valueKey];
+      }
+    });
+  }
 
   return ret;
 }
